85043884c92d7b89e66e52523e7ad74bad9d9aa7
BjornMelgaard <melgaardbjorn@gmail.com>
2018-05-17 22:02:31 +0300

    feat: add docker-env script

---
+++ docker-env.sh
@@ -0,0 +1,8 @@
+docker run \
+  -it \
+  --rm \
+  --name "$(basename $PWD)-ruby23" \
+  -v `pwd`:/usr/src/app \
+  -w /usr/src/app \
+  ruby:2.3 \
+  "bundle install && npm install -g mjml && bash"
